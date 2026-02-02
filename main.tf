6ba1161f84014dce9d522cb73001dc33952b8a33
zabronax <zabronax@gmail.com>
2026-02-02 10:25:55 +0100

    define remote repository

--- main.tf
+++ main.tf
@@ -7,11 +7,19 @@ terraform {
   }
 }
 
+variable "gh_token" {
+  sensitive = true
+}
+
 provider "github" {
-  # Configuration options
+  token = var.gh_token
 }
 
 resource "github_repository" "primary_repo" {
   name = "2026-backend-advanced-iac-sandbox"
   visibility = "private"
+}
+
+output "github_repo" {
+  value = github_repository.primary_repo.html_url
 }
